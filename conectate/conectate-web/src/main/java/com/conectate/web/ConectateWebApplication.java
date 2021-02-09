ed77a1d4d8dedbc0b54ba4d1f2e9df5fe07edf4f
MiguelZorrox <mecatr_onics@hotmail.com>
2021-02-08 23:40:02 -0600

    Se agrega back common , service, data , web del proyecto conectate
    Se agrega back common , service, data , web del proyecto conectate

---
+++ conectate/conectate-web/src/main/java/com/conectate/web/ConectateWebApplication.java
@@ -0,0 +1,13 @@
+package com.conectate.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConectateWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConectateWebApplication.class, args);
+	}
+
+}
---
+++ conectate/conectate-web/src/test/java/com/conectate/web/ConectateWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.conectate.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConectateWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
